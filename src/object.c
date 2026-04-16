487fe4609f0c89b8c7d18c56f8995c66b975da01
BC2017 <corekinben@gmail.com>
2026-04-16 12:43:25 -0700

    Part 1 Functions and Calls
    Laying the groundwork for implementing functions. Add a CallFrame to the VM and rewrote the VM to be able to run functions.

--- src/object.c
+++ src/object.c
@@ -8,6 +8,9 @@
 #include "value.h"
 #include "vm.h"
 
+#define ALLOCATE_OBJ(type, objectType) \
+  (type*)allocateObject(sizeof(type), objectType)
+
 static Obj* allocateObject(size_t size, ObjType type) {
   Obj* object = (Obj*)reallocate(NULL, 0, size);
   object->next = vm.objects;
@@ -17,9 +20,13 @@ static Obj* allocateObject(size_t size, ObjType type) {
   return object;
 }
 
-#define ALLOCATE_OBJ(type, objectType) \
-  (type*)allocateObject(sizeof(type), objectType)
-
+ObjFunction* newFunction() {
+  ObjFunction* function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+  function->arity = 0;
+  function->name = NULL;
+  initChunk(&function->chunk);
+  return function;
+}
 
 static ObjString* allocateString(char* chars, int length, uint32_t hash) {
   ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
@@ -62,8 +69,19 @@ ObjString* copyString(const char* chars, int length) {
   return allocateString(heapChars, length, hash);
 }
 
+static void printFunction(ObjFunction* function) {
+  if (function->name == NULL) {
+    printf("<script>");
+    return;
+  }
+  printf("<fn %s>", function->name->chars);
+}
+
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
+    case OBJ_FUNCTION:
+      printFunction(AS_FUNCTION(value));
+      break;
     case OBJ_STRING:
       printf("%s", AS_CSTRING(value));
       break;
